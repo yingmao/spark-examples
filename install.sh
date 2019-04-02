207643670a3374b8727691dc3127feeebea9a69c
Ying Mao <giggle.my@gmail.com>
2019-04-01 21:58:24 -0400

    first version

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-python3 /spark-install/setup.py
+python3 /spark-examples/setup.py
 cat workers | while read line
 do
     if [ "$line" = "-" ]; then
         echo "Skip $line"
     else
-        ssh root@$line -n "rm -rf /spark-install/ && mkdir /spark-install/"
+        ssh root@$line -n "rm -rf /spark-examples/ && mkdir /spark-examples/"
         echo "Copy data to $line"
-        scp  /spark-install/setup.py root@$line:/spark-install/ && scp /spark-install/manager root@$line:/spark-install/ && scp /spark-install/workers root@$line:/spark-install/
+        scp  /spark-examples/setup.py root@$line:/spark-examples/ && scp /spark-examples/manager root@$line:/spark-examples/ && scp /spark-examples/workers root@$line:/spark-examples/
         echo "Setup $line"
-        ssh root@$line -n "cd /spark-install/ && python3 setup.py"
+        ssh root@$line -n "cd /spark-examples/ && python3 setup.py"
         echo "Finished config node $line"
     fi
 done
 
-manager=$(cat /spark-install/manager)
+manager=$(cat /spark-examples/manager)
 echo "export SPARK_MASTER=$manager" > env.sh
